60881721111797c6e50029733a31682864779313
April Howard <ahoward@zipcodes-MBP-5.fios-router.home>
2020-03-25 22:49:45 -0400

    answer 2

---
+++ answers/exercise2.sql
@@ -0,0 +1,5 @@
+SELECT * FROM Enrollments JOIN Students ON Enrollments.StudentId = Students.StudentId;
+SELECT * FROM Enrollments INNER JOIN Students ON Enrollments.StudentID = Students.StudentID;
+
+
+
